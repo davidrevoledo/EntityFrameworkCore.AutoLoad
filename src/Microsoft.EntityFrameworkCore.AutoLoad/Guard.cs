73818b0b523fe8b26fdc5ca1c755850db1026c75
David Revoledo <davidrevoledo@d-genix.com>
2018-10-24 00:32:17 -0300

    Initial Commit

---
+++ src/Microsoft.EntityFrameworkCore.AutoLoad/Guard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.EntityFrameworkCore.AutoLoad
+{
+    public static class Guard
+    {
+        public static void ArgumentNotNull(string argumentName, object value)
+        {
+            if (value == null) throw new ArgumentNullException(argumentName);
+        }
+    }
+}
---
+++ src/Microsoft.EntityFrameworkCore.AutoLoad/ModelBuilderExtensions.cs
@@ -0,0 +1,88 @@
+﻿/*
+ *  MIT License
+    Copyright (c) 2017 David Revoledo
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    // Project Lead - David Revoledo davidrevoledo@d-genix.com
+ */
+namespace Microsoft.EntityFrameworkCore
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.EntityFrameworkCore.AutoLoad;
+
+    public static class ModelBuilderExtensions
+    {
+        public static void AddFromAssembly(this ModelBuilder modelBuilder, Assembly assembly)
+        {
+            Guard.ArgumentNotNull(nameof(assembly), assembly);
+
+            // base interface that entity framework core implements for all the map
+            var mappingInterface = typeof(IEntityTypeConfiguration<>);
+
+            // get all candidates to execute the load
+            var mappingTypes = assembly.GetTypes()
+                .Where(x => !x.IsAbstract &&
+                            !x.IsInterface &&
+                            x.GetInterfaces()
+                             .Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == mappingInterface));
+
+            // Get the generic Entity method of the ModelBuilder type
+            var entityMethod = typeof(ModelBuilder).GetMethods()
+                .Single(x => x.Name == "Entity" &&
+                        x.IsGenericMethod &&
+                        x.ReturnType.Name == "EntityTypeBuilder`1");
+
+            foreach (var mappingType in mappingTypes)
+            {
+                // Get the type of entity to be mapped
+                var genericTypeArg = mappingType.GetInterfaces()
+                    .Single()
+                    .GenericTypeArguments
+                    .Single();
+
+                // Get the method builder.Entity<TEntity>
+                var genericEntityMethod = entityMethod.MakeGenericMethod(genericTypeArg);
+
+                // Invoke builder.Entity<TEntity> to get a builder for the entity to be mapped
+                var entityBuilder = genericEntityMethod.Invoke(modelBuilder, null);
+
+                // Create the mapping type and do the mapping
+                var mapper = Activator.CreateInstance(mappingType);
+
+                if (mapper == null)
+                {
+                    continue;
+                }
+
+                // if the method changes in ef core then it will break
+                var method = mappingType.GetMethod("Configure");
+
+                if (method == null)
+                {
+                    continue;
+                }
+
+                // invoke the map 
+                method.Invoke(mapper, new object[] { entityBuilder });
+            }
+        }
+    }
+}
